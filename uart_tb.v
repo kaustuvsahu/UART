53a024367aa66bc0d036871564ac129b5b602bd4
Kaustuv Sahu <34069703+kaustuvsahu@users.noreply.github.com>
2020-06-08 12:38:47 +0530

    Update uart_tb.v

--- uart_tb.v
+++ uart_tb.v
@@ -1,27 +1,4 @@
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   20:43:14 06/06/2020
-// Design Name:   UART_Receiver
-// Module Name:   C:/Users/kaust/Documents/Vivado/ISE/UART/uart_tb.v
-// Project Name:  UART
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: UART_Receiver
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module uart_tb;
   parameter c_CLOCK_PERIOD_NS = 1000;
   parameter c_CLKS_PER_BIT    = 104;//(Clock Frequency/Baud Rate)
