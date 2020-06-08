0c417b2846d8dcc4520c442d8e828defae9cbd6a
Kaustuv Sahu <34069703+kaustuvsahu@users.noreply.github.com>
2020-06-08 12:39:13 +0530

    Update UART_Transmitter.v

--- UART_Transmitter.v
+++ UART_Transmitter.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:31:41 06/06/2020 
-// Design Name: 
-// Module Name:    UART_Transmitter 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module UART_Transmitter
 #(parameter Clk_per_bit=32)
 (
